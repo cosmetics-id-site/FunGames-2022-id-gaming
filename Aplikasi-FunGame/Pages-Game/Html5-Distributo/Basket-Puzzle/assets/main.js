8cfa2d7fa4fffd543392c3e108339ef654f15b42
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-03-15 10:57:20 +0700

    first commit

--- Aplikasi-FunGame/Pages-Game/Html5-Distributo/Basket-Puzzle/assets/main.js
+++ Aplikasi-FunGame/Pages-Game/Html5-Distributo/Basket-Puzzle/assets/main.js
@@ -1,2 +1,2 @@
 
-    document.getElementById("FunGames").src ="https://html5.gamedistribution.com/01a28589cb4c4b949ea08c8de26a8ee7/";
+    document.getElementById("FunGames").src ="https://www.addictinggames.com/embed/html5-games/19593";
